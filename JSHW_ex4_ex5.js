878265e65ccc2ec7563c99e392b95bfe3f3a4549
Daria <daryayo@yandex.ru>
2023-02-08 17:33:18 +0300

    ready for check

--- JSHW_ex4_ex5.js
+++ JSHW_ex4_ex5.js
@@ -16,8 +16,11 @@ class ElectronicDevice {
    
   
   }
-  class BedroomDevice extends ElectronicDevice {
-   
+  class RoomDevice extends ElectronicDevice {
+   constructor(name,power,room){
+     super(name,power);
+     this.room = room;
+   }
   
    makeNoize(){ 
     if (this.isSwitchOn){
@@ -26,30 +29,55 @@ class ElectronicDevice {
   }
   }
   
+  class KitchenDevice extends RoomDevice{
+    constructor(name, power, room, needWater)
+    {
+      super(name,power,room);
+      this.isNeedWater = needWater;
+    }
     
+    giveWater(){
+      if (this.isSwitchOn && this.isNeedWater ){
+  
+        console.log (`${this.name} -  готовит ваш напиток`);
+      } else if (this.isSwitchOn && !this.isNeedWater){
+        console.log(`Лучше не заливать воду в ${this.name} `)
+      }
+    } 
+  }
   
   
-  const laptop = new ElectronicDevice("laptop",80),
-        lamp = new ElectronicDevice("lamp",10),
-        tv = new ElectronicDevice("tv",120),
-        dishWasher = new ElectronicDevice("dishWasher", 100);
+  const lamp = new ElectronicDevice("lamp",10),
+        hoover = new ElectronicDevice("tv",120);
+        
   
-  const radio = new BedroomDevice("radio",40);
+  const radio = new RoomDevice("radio",40,"bedroom"),
+        laptop = new RoomDevice("laptop",80,"bedroom"),
+        tv = new  RoomDevice("TV",80,"bedroom");
   
+  const  coffeMachine = new KitchenDevice("coffemachine", 80, "kitchen", true),
+         mixer = new KitchenDevice("cocktail's maker", 85, "kitchen", true),
+         fridge = new KitchenDevice("fridge", 50, "kitchen", false);
   
   radio.switchOn()
   radio.makeNoize()
   
   lamp.switchOn()
   
-  tv.switchOn()
+  coffeMachine.switchOn()
+  coffeMachine.giveWater()
+  
+  fridge.switchOn()
+  fridge.giveWater()
   
   
   let getTotalPower = function(){
-    console.log(`Сейчас потребляется ${laptop.getPower()+lamp.getPower() + tv.getPower()+ dishWasher.getPower() + radio.getPower()} Ватт`)
+    console.log(`Сейчас потребляется ${laptop.getPower()+lamp.getPower() + tv.getPower()+ hoover.getPower() + radio.getPower()+ coffeMachine.getPower()+ fridge.getPower() + mixer.getPower()} Ватт`)
   };
   
   
   getTotalPower()
+  
+  
   
   
